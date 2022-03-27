3fe1ba80a1c4c9fc99020efe5f11004a4708285d
Steven Kravitz <Steven.Kravitz95@gmail.com>
2022-03-27 01:10:21 -0400

    renamed models to match Social Network needs. Built User Model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+
+// Email validation regex
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+}
+
+// Schema to create a course model
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    },
+    email: {
+      type: String,
+      default: true,
+      trim: true,
+      unique: true,
+      validate: [validateEmail, 'Please enter a valid email address.']
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
